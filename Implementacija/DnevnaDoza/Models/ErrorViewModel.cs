d2b9aeebc85b57b0c3ca62e6870acee5bfe1f132
zpandza1 <zpandza1@etf.unsa.ba>
2025-04-15 10:01:07 +0200

    initial commit

---
+++ Implementacija/DnevnaDoza/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DnevnaDoza.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
