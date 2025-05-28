c42b7bdba3b20a97fce2122a7657f9c29099647d
mogric <mogric1@etf.unsa.ba>
2025-05-28 16:10:12 +0200

    Moje novo

--- Implementacija/DnevnaDoza/obj/Debug/net8.0/DnevnaDoza.AssemblyInfo.cs
+++ Implementacija/DnevnaDoza/obj/Debug/net8.0/DnevnaDoza.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DnevnaDoza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e466d8a25b75f59a026e1364a9c9c7ded87b29a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7e38e3ae3e154b7331bf9228b742df97e300c21")]
 [assembly: System.Reflection.AssemblyProductAttribute("DnevnaDoza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DnevnaDoza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
