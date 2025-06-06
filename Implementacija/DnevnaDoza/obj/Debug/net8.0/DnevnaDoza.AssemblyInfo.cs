471957726b9d8a9a38b1834d82504100016a253b
SumejaMusic <smusic1@etf.unsa.ba>
2025-06-07 01:13:49 +0200

    a

--- Implementacija/DnevnaDoza/obj/Debug/net8.0/DnevnaDoza.AssemblyInfo.cs
+++ Implementacija/DnevnaDoza/obj/Debug/net8.0/DnevnaDoza.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DnevnaDoza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59e507153e8773fea54aaa0e9f260a660a3b8a98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74202da6d51798d92829ca6d3487304b071ba882")]
 [assembly: System.Reflection.AssemblyProductAttribute("DnevnaDoza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DnevnaDoza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
